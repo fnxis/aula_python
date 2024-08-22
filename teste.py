9cdc762eb71c42f7dfb2884fd141d6bbda6a9ae0
fnxis <guiclash4759@gmail.com>
2024-08-22 20:40:12 -0300

    calculadora

---
+++ teste.py
@@ -0,0 +1,25 @@
+var1= input("?")
+var2= 7
+var3= 3
+
+
+
+if var1 == "soma":
+    def somar(x,y):
+        return x+y
+    print(f"a soma é: {somar(var2,var3)}")
+    
+if var1 == "subi" :
+    def sub(x,y):
+        return x-y
+    print(f"a subtraçao é: {sub(var2,var3)}")
+    
+if var1 == "mult" :
+    def multi(x,y):
+        return x*y
+    print(f"a multiplicaçao é: {multi(var2,var3)}")
+
+if var1 == "div" :
+    def divi(x,y):
+        return x/y
+    print(f"a divisao é: {divi(var2,var3)}")
