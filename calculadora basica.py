a8f807bf8b78473d32cfa9f32bdcb27546d99447
fnxis <guiclash4759@gmail.com>
2024-08-23 17:23:49 -0300

    quadratica

--- calculadora basica.py
+++ calculadora basica.py
@@ -1,14 +1,14 @@
-var1= input("?")
-var2= 27
-var3= 41
-
+var1= input("digite sua funçao:")
+var2= int(input("digite seu numero:"))
+var3= int(input("digite seu numero:"))
+var4= int(input("digite seu numero:"))
 
 
 if var1 == "soma":
     def somar(x,y):
         return x+y
-    print(f"a soma é: {somar(var2,var3)}")
-    
+    print(f"a soma é: {somar (var2,var3)}")
+
 if var1 == "subi" :
     def sub(x,y):
         return x-y
@@ -17,7 +17,12 @@ if var1 == "subi" :
 if var1 == "mult" :
     def multi(x,y):
         return x*y
-    print(f"a multiplicaçao é: {multi(var2,var3)}")
+    print (f"a multiplicaçao é: {multi(var2,var3)}")
+
+if var1 =="quadratica":
+    def quadrado(x,y,z):
+        return y*y -4*x*z
+    print (f"a funçao quadratica é: {quadrado(var2,var3,var4)}")
 
 if var1 == "div" :
     def divi(x,y):
