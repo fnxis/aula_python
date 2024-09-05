20f841adff363118cdaa5d780765844d36b79d86
fnxis <guiclash4759@gmail.com>
2024-09-05 20:13:34 -0300

    aprender a fatiar o print

---
+++ aulaNova.py
@@ -0,0 +1,8 @@
+texto="ola mundo"
+#fatiar a string de acordo com seus comandos
+print(texto[:])
+print(texto[:3])
+print(texto[4:9])
+print(texto[::-1])
+print(texto[::2])
+#(texto[escolher onde começa:escolher onde termina:escolher de quantos em quantos digitos o codigo irá ler])
