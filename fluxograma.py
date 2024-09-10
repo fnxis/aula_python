a8a0442b088c155ac903b58e31a79975b740ba12
fnxis <guiclash4759@gmail.com>
2024-09-10 20:37:43 -0300

    fluxograma de acordar

---
+++ fluxograma.py
@@ -0,0 +1,21 @@
+var1=["escova os dentes","toma cafe","dorme denovo"]
+print("oque voçe faz quando acorda?",var1)
+var2=int(input(":"))
+var3=["quer tomar cafe?","ja escovou os dentes?"]
+
+
+
+if var2 == 1: 
+    print("voçe esta bem alimentado")
+if var2 == 0:
+    print("seus dentes estao lindos")
+if var2 == 2:
+    print("voçe esta bem preguiçoso")
+
+def validarchamda2(var2):
+    if var2 :1 or 0
+    print(input("oque vai fazer agora?:"))
+    print("tenha um otimo dia!!")
+categoriaIdade = validarchamda2(var2)
+print(categoriaIdade)
+
