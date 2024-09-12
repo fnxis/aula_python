fe79b625ab8f86141411cab2476c6cf66bbf3f95
fnxis <guiclash4759@gmail.com>
2024-09-12 19:25:02 -0300

    ultimo desafio

--- Desafio/desafio7.py
+++ Desafio/desafio7.py
@@ -1,6 +1,13 @@
 alunos={"ryan":2,"galvao":8,"rafael":7,"guilherme":10}
-print(alunos)
 verificaraluno=input("qual aluno voçe quer? ")
 if verificaraluno in alunos:
-    print(verificaraluno[1])
-    
+    if verificaraluno =="rafael":
+        print(f"a sua nota é {alunos['rafael']}")
+    if verificaraluno =="ryan":
+        print(f"a sua nota é {alunos['ryan']}")
+    if verificaraluno =="guilherme":
+        print(f"a sua nota é {alunos['guilherme']}")
+    if verificaraluno =="galvao":
+        print(f"a sua nota é {alunos['galvao']}")        
+else:
+    print("este aluno nao esta com nota")
